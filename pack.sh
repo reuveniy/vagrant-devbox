4b582bb94bd3e327a0dbfc24fea9bab6756402f1
Yoseph Reuveni <yoseph@jet.com>
2016-05-23 11:28:05 -0400

    fix ubuntu vagrant box version issues

--- pack.sh
+++ pack.sh
@@ -11,7 +11,7 @@ sleep 10
 vagrant halt
 
 export BOX="dev-basebox"
-export BOX_VER="$(vagrant box list | grep "ubuntu/xenial64" | tail -1 | awk '{print $3}' | sed 's/)//g')"
+export BOX_VER="$(vagrant box list | grep "ubuntu/xenial64" | tail -1 | awk '{print substr($3,0,index($3,")")-1)}')"
 export BOX_FILE="${HOME}/.vagrant.d/boxes/ubuntu-VAGRANTSLASH-xenial64/${BOX_VER}/virtualbox/Vagrantfile"
 cat ${BOX_FILE} | sed 's/ubuntu-xenial-16.04-cloudimg/dev-basebox/g' | tee Vagrantfile.box
-vagrant package ubuntu --output ${BOX}.box --vagrantfile Vagrantfile.box && vagrant box add ${BOX}.box --name ${BOX} --force && rm -f ${BOX}.box && rm *.log
+vagrant package ubuntu --output ${BOX}.box --vagrantfile Vagrantfile.box && vagrant box add ${BOX}.box --name ${BOX} --force && rm -f ${BOX}.box && rm -f *.log
